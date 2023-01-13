d26ed372a3daefc23af3b1192aeb551704535052
Nanda Aditya Putra <nandaditra56@gmail.com>
2023-01-13 14:55:14 +0700

    dito answer

---
+++ dito.js
@@ -0,0 +1,13 @@
+//javascript
+//code 
+
+let input = 1111
+let stringInt = ""
+
+for(let i =1;i<=input;i++) {
+    stringInt += i
+}
+
+let searchIndexQueston = input-1
+
+console.log(stringInt[searchIndexQueston])
